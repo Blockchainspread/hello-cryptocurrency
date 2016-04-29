ca105acac690c1d1a9a68a7e42451bde815d1e7c
GameCredits <asksecinfo@gmail.com>
2016-04-29 19:11:11 +0200

    up

--- update.sh
+++ update.sh
@@ -1,25 +1,29 @@
+#!/bin/bash
+
+#./update.sh coinname
+
 #Clean old Debian package Repo
 rm debian/Packages.gz
 
-#Build Gamecredits Debian package
-rm debian/gamecredits_i386.deb
-chmod 0555 coins/gamecredits/DEBIAN/postinst
-chmod 0777 coins/gamecredits/usr/bin/gamecreditsd
-chmod 0777 coins/gamecredits/usr/bin/gamecredits-cli
-dpkg-deb --build coins/gamecredits
-mv coins/gamecredits.deb gamecredits_i386.deb
-cp gamecredits_i386.deb debian/
-rm gamecredits_i386.deb
+#Build Debian package
+rm debian/$1_i386.deb
+chmod 0555 coins/$1/DEBIAN/postinst
+chmod 0777 coins/$1/usr/bin/$1d
+chmod 0777 coins/$1/usr/bin/$1-cli
+dpkg-deb --build coins/$1
+mv coins/$1.deb $1_i386.deb
+cp $1_i386.deb debian/
+rm $1_i386.deb
 
-#Build OkCash Debian package
-rm debian/okcash_i386.deb
-chmod 0555 coins/okcash/DEBIAN/postinst
-chmod 0777 coins/okcash/usr/bin/okcashd
-chmod 0777 coins/okcash/usr/bin/okcash
-dpkg-deb --build coins/okcash
-mv coins/okcash.deb okcash_i386.deb
-cp okcash_i386.deb debian/
-rm okcash_i386.deb
+#Build Debian package 
+rm debian/$1_armel.deb
+chmod 0555 coins/$1/DEBIAN/postinst
+chmod 0777 coins/$1/usr/bin/$1d
+chmod 0777 coins/$1/usr/bin/$1-cli
+dpkg-deb --build coins/$1-arm
+mv coins/$1-arm.deb $1_armel.deb
+cp $1_armel.deb debian/
+rm $1_armel.deb
 
 #Build Packages.gz
 dpkg-scanpackages debian /dev/null | gzip -9c > debian/Packages.gz
